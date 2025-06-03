908461b2723399a44893951fb51fa896a9cb15b1
pedro henrique <pedroh20066@gmail.com>
2025-06-03 13:50:25 -0300

    fix add css

--- ProjetinhoMódulo1,2,3/script.js
+++ ProjetinhoMódulo1,2,3/script.js
@@ -5,17 +5,17 @@ var text2 = document.getElementById('txt2');
 text2.innerHTML = `Fahreinht:`
 
 
-document.getElementById('Converter').addEventListener('click', function(e){
-    e.preventDefault();
-    if(text1.innerHTML == `Celsius:`){
-        var val1 = parseFloat(document.getElementById('valor1').value);
-        var resultado = (val1 * 1.8) + 32;
-    }else{
-        var val1 = parseFloat(document.getElementById('valor1').value);
-        var resultado = (val1 - 32) * 5/9;
-    }
-    document.getElementById('valor2').value = resultado.toFixed(2);
-})
+    document.getElementById('Converter').addEventListener('click', function(e){
+        e.preventDefault();
+        if(text1.innerHTML == `Celsius:`){
+            var val1 = parseFloat(document.getElementById('valor1').value);
+            var resultado = (val1 * 1.8) + 32;
+        }else{
+            var val1 = parseFloat(document.getElementById('valor1').value);
+            var resultado = (val1 - 32) * 5/9;
+        }
+        document.getElementById('valor2').value = resultado.toFixed(2);
+    })
 
 document.getElementById('Inverter').addEventListener('click', function(e){
     e.preventDefault();
